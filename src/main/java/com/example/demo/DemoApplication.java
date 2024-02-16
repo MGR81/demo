99d44b50fe0ce368ad013b8acd72ebbf6cc21d28
MGR81 <154337425+MGR81@users.noreply.github.com>
2024-02-16 23:59:02 +0000

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,7 @@ public class DemoApplication {
 
    private static final String MESSAGE = "Hello World!";
 
-    public App() {}
+    public DemoApplication() {}
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
